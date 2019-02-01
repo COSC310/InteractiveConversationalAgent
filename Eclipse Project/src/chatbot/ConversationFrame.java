f8ed4eb9c4554709f0f8bd13de3f9a92bf7c28d3
Courtney Gosselin <courtneygosselin1996@gmail.com>
2019-01-31 19:40:18 -0800

    Test code
    Signed-off-by: Courtney Gosselin <courtneygosselin1996@gmail.com>

---
+++ Eclipse Project/src/chatbot/ConversationFrame.java
@@ -0,0 +1,44 @@
+package ConversationBot;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class ConversationFrame extends JFrame{
+	private Conversation conversation;
+	public ConversationFrame(Conversation conversation, String title, int width, int height) {
+		this.conversation = conversation;
+		//attributes
+		setSize(width, height);
+		setTitle(title);
+		setUndecorated(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		//create and add objects
+		add(new ConversationPanel());
+	}
+	
+	private class ConversationPanel extends JPanel{
+		BufferedImage myImage;
+		public ConversationPanel() {
+			//setBackground(Color.BLACK);
+			try {
+				myImage = ImageIO.read(new File("background.jpg"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		public void paint(Graphics g) {
+			super.paint(g);
+			g.drawImage(myImage, 0, 0, this);
+			conversation.paint((Graphics2D)g);
+		}
+	}
+
+}
--- Eclipse Project/src/chatbot/YeBot.java
+++ Eclipse Project/src/chatbot/YeBot.java
@@ -1,8 +1,10 @@
-package chatbot;
+import java.util.*;
+
+package src.chatbot;
 
 public class YeBot {
 	
-	private java.util.Scanner client = new java.util.Scanner(System.in); //change input type if needed
+	private Scanner client = new Scanner(System.in); //change input type if needed
 	
 	public void start() {
 		//change to end when gui is closed
@@ -12,7 +14,45 @@ public class YeBot {
 	}
 	
 	public void talk() {
-		String input = client.nextLine();
-		System.out.println("I am YE   bow down to you you mortal peasants");
+		static Conversation conversation;			
+		
+		public static void main(String[] args) {
+			String ans;
+			do{								
+			//Reset the conversation
+				conversation = new Conversation();			
+				
+				String input = null;
+		
+				//	set a default max number of response to 5 currently
+				int maxResponses=5;	
+				//cycle through a conversation with max responses 
+				for(int i=0;i<maxResponses;i++){	
+					//write code to handle user input
+					//just a default for testing user interface
+					if(i==0 || input==null || input==""){
+						//start the conversation
+						input = conversation.response("Hello");
+					}else if(input.toLowerCase().equals("hi")) {
+						input=conversation.response("How are you?");
+					}else if(input.toLowerCase().equals("good. how are you?")) {
+						input=conversation.response("Good");
+					}else if(input.toLowerCase().equals("what is your name?")) {
+						input=conversation.response("Kanye! What is your name?");
+					}else {
+						input=conversation.response("You said \"" + input + "\"");
+					}
+				}	
+			
+				//Do you want to start the conversation over? make sure you get valid input (Done)
+				ans = conversation.response("Do you want to start our conversation over? (Y/N)"); 
+			while(ans != null && !ans.toUpperCase().equals("Y") && !ans.toUpperCase().equals("N"))
+				ans = conversation.response("Invalid input. Do you want to start our conversation over? (Y/N)");
+			}while(ans.toUpperCase().equals("Y"));	//start over if answer is "Y" or "y"
+
+			System.exit(1); 	//This statement terminates the program	
+		}
+		
+		
 	}
 }
